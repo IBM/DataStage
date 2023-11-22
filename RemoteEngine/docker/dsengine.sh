ddd905e6093af35b7a040882b2e1007dad9652b1
rohit <rgargate@us.ibm.com>
2023-11-22 12:09:08 -0800

    switch permissions to 775
    Signed-off-by: rohit <rgargate@us.ibm.com>

--- RemoteEngine/docker/dsengine.sh
+++ RemoteEngine/docker/dsengine.sh
@@ -357,7 +357,7 @@ create_dir_if_not_exist() {
     if [ ! -d $DIR_PATH ]; then
         echo "Folder ${DIR_PATH} does not exist, creating ..."
         mkdir -p "${DIR_PATH}"
-        chmod 777 "${DIR_PATH}"
+        chmod -R 775 "${DIR_PATH}"
     fi
 }
 
@@ -533,7 +533,7 @@ run_px_runtime_docker() {
     fi
 
     # once the container is up, reset permissions on the volumes dir
-    chmod -R 777 "${DOCKER_VOLUMES_DIR}"
+    chmod -R 775 "${DOCKER_VOLUMES_DIR}"
 
     # wait until docker is in a running state - doesn't mean server has started
     until [[ $( $DOCKER_CMD ps -a | grep $PXRUNTIME_CONTAINER_NAME | wc -l ) -gt 0 ]]; do
@@ -1061,7 +1061,7 @@ setup_docker_volumes() {
             create_dir_if_not_exist "${PX_STORAGE_HOST_DIR}"
             create_dir_if_not_exist "${PX_STORAGE_WLM_DIR}"
             create_dir_if_not_exist "${SCRATCH_DIR}"
-            chmod -R 777 "${DOCKER_VOLUMES_DIR}"
+            chmod -R 775 "${DOCKER_VOLUMES_DIR}"
         fi
 
         # Mount an empty file for running computes since they aren't supported locally
@@ -1137,7 +1137,7 @@ startCassandraSQLENgine
 startMongoDBSQLEngine
 startContainer
 EOL
-chmod 777 "${STARTUP_FILE}"
+chmod 775 "${STARTUP_FILE}"
     fi
 }
 
@@ -1172,7 +1172,7 @@ else
 echo "SNC related environment variables must be set by user to proceed. Aborting SNC configuration."
 fi
 EOL
-    chmod 777 "${SNC_FILE}"
+    chmod 775 "${SNC_FILE}"
     fi
 }
 
@@ -1205,7 +1205,7 @@ LOG_RETENTION_RUNS = 10
 # The frequency of log retention manager in hours
 LOG_MANAGER_FREQ = 24
 EOL
-    chmod 777 "${LOG_CFG_FILE}"
+    chmod 775 "${LOG_CFG_FILE}"
     fi
 }
 
@@ -1333,7 +1333,7 @@ if [[ -z "\${DS_PX_COMPUTE_REPLICAS}" ]]; then
   fi
 fi
 EOL
-    chmod 777 "${INIT_VOL_FILE}"
+    chmod 775 "${INIT_VOL_FILE}"
     fi
 }
 
